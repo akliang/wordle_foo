c281a18874e9509bce4b6c4ef614689b72c82971
akliang <none@none.com>
2022-01-19 17:31:37 +0000

    reorg code for clarity

--- js/messages.js
+++ js/messages.js
@@ -2,6 +2,8 @@ app.component('messages', {
   template: `
     <div class="message-wrapper" v-bind:class="this.$store.state.gameSettings.messageColor">
       {{ this.$store.state.gameSettings.message }}
-      <span v-if="this.$store.state.gameSettings.gameWon"><a href="#" @click="initializeGame()">Play again?</a></span>
+      <span v-if="this.$store.state.gameSettings.gameWon">
+        <a href="#" @click="initializeGame()">Play again?</a>
+      </span>
     </div>`,
 })
