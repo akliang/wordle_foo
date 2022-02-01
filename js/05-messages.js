e1c430b272942f5e6297d65442f08c3fdbcbe730
akliang <none@none.com>
2022-02-01 16:03:15 +0000

    fixed bug in share button id

--- js/05-messages.js
+++ js/05-messages.js
@@ -4,7 +4,7 @@ app.component('messages', {
       {{ this.$store.state.gameSettings.message }}
     
       <template v-if="this.$store.state.gameSettings.playAgain">
-        <button @click="shareOut()" class="bg-orange-400 text-white px-2 py-0.5 rounded-md mr-2" id="play-again">Share <i class="fas fa-share-square"></i></button>
+        <button @click="shareOut()" class="bg-orange-400 text-white px-2 py-0.5 rounded-md mr-2" id="share-out">Share <i class="fas fa-share-square"></i></button>
         <button @click="initializeGame()" class="bg-sky-600 text-white px-2 py-0.5 rounded-md" id="play-again">Play again?</button>
       </template>
     </span>`,
